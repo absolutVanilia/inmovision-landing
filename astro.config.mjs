d0e81e6d46fc67c1c28788566fce6c6e3b3a4ba6
alejandrolcstaion <alejandrolcstaion@gmail.com>
2026-03-25 18:25:52 -0500

    [build] Add Astro tunnel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import tunnel from 'astro-tunnel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [tunnel()]
 });
